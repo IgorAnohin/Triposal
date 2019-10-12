2cf1ff89f339a4a2af8f1ca7ec33b7360befb896
Henok <henok@client62-10-192-16.eduroam.upc.edu>
2019-10-12 07:09:43 +0200

    fixes score calculating method

--- server/modules/get_score.py
+++ server/modules/get_score.py
@@ -7,24 +7,24 @@ class ScoreCalculator():
     def __init__(self, answers):
         self.cities_collection = CitiesCollection()
         self.answers = answers
-        scores = pd.DataFrame(
-            {'score': [0] * 730})
-        self.new_data = pd.concat([self.cities_collection.data, scores])
-        self.new_data.to_csv("test2.csv")
+        self.new_data = self.cities_collection.data
+        self.scores = [0]*self.new_data.shape[0]
+        
 
     def get_city_recommendation(self, n_cities=10):
         for index, row in self.new_data.iterrows():
             for key, value in self.answers.items():
                 try:
-                    row['score'] += int(
-                        row[self.cities_collection._old_new_features_mapping[key]]) - int(value)
+                    self.scores[index] += abs(int(row[self.cities_collection._old_new_features_mapping[key]]) - int(value))
+                    print(self.scores[index], int(row[self.cities_collection._old_new_features_mapping[key]]), int(value))
                 except ValueError as e:
-                    row['score'] += int(value)
+                    self.scores[index] += int(value)
 
         # self.new_data.to_csv("test.csv")
+        self.scores.reverse()
         cities = np.argpartition(
-            self.new_data['score'].to_numpy(), -1*n_cities)[-1*n_cities:]
-        
+            np.array(self.scores), -1*n_cities)[-1*n_cities:]
+        print(self.scores)
         cities = np.array(self.cities_collection.get_cities())[cities]
 
         print(cities)
